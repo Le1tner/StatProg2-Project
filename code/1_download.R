ef3eb4a4dce5c4b540112daf653a8a9804e5e132
Le1tner <leitner.business@gmail.com>
2026-07-03 15:41:29 +0200

    added download script

--- code/1_download.R
+++ code/1_download.R
@@ -4,7 +4,7 @@
 
 library(here)
 
-# url <- "https://..."
-# download.file(url, destfile = here("data", "raw", "data.csv"))
+url =  "https://sdmx.oecd.org/public/rest/data/OECD.STI.DEP,DSD_ICT_HH_IND@DF_IND,1.1/DEU+FRA+USA.A........?startPeriod=2021&endPeriod=2025&dimensionAtObservation=AllDimensions&format=csvfilewithlabels"
+download.file(url, destfile = here("data", "raw", "data.csv"))
 
 message("Raw data already present — delete data/raw/ and re-run to refresh.")
